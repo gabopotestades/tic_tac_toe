318db17647d485c0a979ac8d8f7214e5b9adae3d
gabopotestades <grpotestades@gmail.com>
2020-08-08 23:52:31 +0800

    Added grid draft

--- GameWindow.py
+++ GameWindow.py
@@ -2,16 +2,51 @@ import tkinter as tk
 
 class Game:
     def __init__(self, master, rationality):
+        
+        #Setup grid
         self.master = master
         self.rationality = rationality
         self.master.title("Tic Tac Toe")
-
-        self.master.minsize(width = 400, height = 400)
         self.master.resizable(width = False, height = False)
-        #self.frame = tk.Frame(self.master)
+        self.canvas = tk.Canvas(self.master, width=600, height=300, borderwidth=0, highlightthickness=0, background="#7693b8")
+        self.canvas.pack(side="top", fill="both", expand="true")
 
-        label = tk.Label(self.master, text=('{0}: {1}').format('Rationality', self.rationality))
-        label.config(font=("Adobe Song Std L", 30))
-        label.grid(row=0, columnspan=4, ipadx=40, ipady=20)
+        #Number of rows and columns
+        self.rows = 3
+        self.columns = 3
+        
+        #List of tiles
+        self.tiles = {}
+        self.canvas.bind("<Configure>", self.redraw)
+        
+        #Update history
+        #self.strActions = tk.StringVar()
+        #self.action_history = tk.Text(self.master, width = 300, height = 300)
+        #self.action_history.pack(side="right")
+        #self.status = tk.Label(self.master, anchor="w")
+        #self.status.pack(side="bottom", fill="x")
+        
+
+    #Function to draw grid
+    def redraw(self, event=None):
+        self.canvas.delete("rect")
+        cellwidth = int((self.canvas.winfo_width() / 2)/self.columns)
+        cellheight = int(self.canvas.winfo_height()/self.columns)
+        for column in range(self.columns):
+            for row in range(self.rows):
+                x1 = column*cellwidth
+                y1 = row * cellheight
+                x2 = x1 + cellwidth
+                y2 = y1 + cellheight
+                tile = self.canvas.create_rectangle(x1,y1,x2,y2, fill="#8cb4d2", tags="rect")
+                self.tiles[row,column] = tile
+                self.canvas.tag_bind(tile, "<1>", lambda event, row=row, column=column: self.clicked(row, column))
+
+    def clicked(self, row, column):
+        tile = self.tiles[row,column]
+        tile_color = self.canvas.itemcget(tile, "fill")
+        new_color = "#8cb4d2" if  tile_color == "#494f83" else "#494f83"
+        self.canvas.itemconfigure(tile, fill=new_color)
+        #self.status.configure(text="you clicked on %s/%s" % (row, column))
 
 
--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ class Main:
     
     def __init__(self, master):
 
+        #Function for button to decide rationality level for next screen
         def choice():
             value = var.get()
             if value == 0:
@@ -14,41 +15,49 @@ class Main:
                 self.newWindow = tk.Toplevel(self.master)
                 self.app = Game(self.newWindow, value)
         
+        #Removes message if level is selected
         def sel():
             message_label.config(text='')
 
         self.master = master
-        #self.frame = tk.Frame(self.master)
         self.master.title("Tic Tac Toe")
+
+        #Set overall color
+        #Taken from https://www.color-hex.com/color-palette/95969
+        self.background_color = "#a2d6ec"
 
         self.master.minsize(width = 200, height = 320)
         self.master.resizable(width = False, height = False)
+        self.master.config(bg = self.background_color)
         var = tk.IntVar()
 
 #Insert objects to the UI
-
         label = tk.Label(self.master, text='Tic Tac Toe')
-        label.config(font=("Adobe Song Std L", 30))
+        label.config(font=("Adobe Song Std L", 30), bg = self.background_color)
         label.grid(row=0, columnspan=4, ipadx=40, ipady=20)
 
         message_label = tk.Label(self.master, text='', fg='red')
-        message_label.config(font=("Goudy Old Style", 13))
+        message_label.config(font=("Goudy Old Style", 13), bg = self.background_color)
         message_label.grid(row=1, columnspan=4)
 
         label = tk.Label(self.master, text='Select Rationality:')
-        label.config(font=("Goudy Old Style", 16))
-        label.grid(row=2, columnspan=4, ipadx=40, ipady=10)
+        label.config(font=("Goudy Old Style", 16), bg = self.background_color)
+        label.grid(row=2, columnspan=4, ipadx=40, ipady=0)
 
         rBtnRandom = tk.Radiobutton(self.master, text = "Level 0 (Random)", variable = var, value = 1, command = sel)
+        rBtnRandom.config(bg = self.background_color, font=("Calibri", 12))
         rBtnRandom.grid(row=3, columnspan=4, ipadx=40, ipady=0)
 
         rBtnHard = tk.Radiobutton(self.master, text = "Level 1 (Hard-coded)", variable = var, value = 2, command = sel)
+        rBtnHard.config(bg = self.background_color, font=("Calibri", 12))
         rBtnHard.grid(row=4, columnspan=4, ipadx=0, ipady=0)
 
         rBtnSearch = tk.Radiobutton(self.master, text = "Level 2 (Search strategy)", variable = var, value = 3, command = sel)
+        rBtnSearch.config(bg = self.background_color, font=("Calibri", 12))
         rBtnSearch.grid(row=5, columnspan=4, ipadx=0, ipady=0)
 
         btnAccept = tk.Button(self.master, text = "Accept", command = choice)
+        btnAccept.config(font=("Calibri", 12))
         btnAccept.grid(row=7, columnspan =4, ipadx = 10, pady=(20, 0))
 
 if __name__ == "__main__":
